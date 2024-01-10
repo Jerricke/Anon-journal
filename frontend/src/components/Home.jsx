6f837d00c05d5b5f444cd7760527fd38639ce390
Jerricke <eejerrickw@gmail.com>
2024-01-09 17:10:04 -0800

    home page reworked

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,32 +1,75 @@
-import { useEffect } from 'react';
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/no-unescaped-entities */
 import './home.css';
-import { useNavigate } from 'react-router-dom';
+import { Typewriter } from 'react-simple-typewriter';
 
 export default function Home() {
-    const navigate = useNavigate();
-
-    const proceed = () => {
-        navigate('/about');
-    };
-
     return (
-        <div className="body" onClick={proceed}>
-            <div className="book">
-                <div className="back" />
-                <div className="page6">
-                    <h1>Click</h1>
-                    <h1>Me</h1>
+        <div className="home">
+            <div className="home__heroContainer">
+                <h1 className="home__title">Anon Journal</h1>
+                <span className="home__hero">
+                    <h1>
+                        A place that lets you <br />
+                        <Typewriter
+                            words={[
+                                'declutter your mind',
+                                'share your thoughts and emotions',
+                                'express yourself',
+                                'organize your thoughts',
+                                'share the canvas of your mind',
+                                "relate to other's experiences",
+                            ]}
+                            typeSpeed={60}
+                            deleteSpeed={50}
+                            loop={false}
+                        />
+                    </h1>
+                </span>
+            </div>
+            <div className="home__aboutContainer">
+                <div>
+                    <h1>About Anon Journal</h1>
+                    <h3>-{'>'} Your Safe Space for Unfiltered Expression!</h3>
+                    <h3>
+                        -{'>'} Dive into the world of anonymous journaling,
+                        where your thoughts and emotions find a voice without
+                        judgment.
+                    </h3>
+                    <h3>
+                        -{'>'} Sign up, pour your heart out, and share your
+                        untold stories with our supportive community. And rest
+                        assured, your identity remains confidential.
+                    </h3>
+                    <h3>
+                        -{'>'} Write up to 1500 words! Keeping things at a good
+                        length!
+                    </h3>
+                    <h3>
+                        -{'>'} Join Anon Journal today and let the power of
+                        anonymity liberate your words!
+                    </h3>
                 </div>
-                <div className="page5" />
-                <div className="page4" />
-                <div className="page3" />
-                <div className="page2" />
-                <div className="page1" />
-                <div className="front">
-                    <h1>Anon</h1>
-                    <h1>Journal</h1>
+            </div>
+            <div className="home__actionContainer">
+                <div>
+                    <h1>
+                        Still unsure? Just give it a try! It's totally free.99
+                    </h1>
+                    <button className="btn" onClick={(e) => {}}>
+                        Let's Go!
+                    </button>
                 </div>
             </div>
         </div>
     );
 }
+
+// Welcome to Anon Journal – Your Safe Space for Unfiltered
+// Expression! Dive into the world of anonymous journaling,
+// where your thoughts and emotions find a voice without
+// judgment. Sign up, pour your heart out, and share your
+// untold stories with our supportive community. Write up to
+// 1500 words, and rest assured, your identity remains
+// confidential. Join Anon Journal today and let the power of
+// anonymity liberate your words!
