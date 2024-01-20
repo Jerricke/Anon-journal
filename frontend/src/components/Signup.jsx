dd1496179cf68119c10bc0adc56e81caa0ec409c
Jerricke <eejerrickw@gmail.com>
2024-01-19 23:40:50 -0800

    signup

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
 
 export default function Signup() {
+    const formSchema = yup.object().shape({
+        username: yup
+            .string()
+            .max(16, 'Must be 16 characters or less')
+            .required('Must enter a username'),
+        password: yup
+            .string()
+            .max(20, 'Must be 20 characters or less')
+            .required('Must enter a password'),
+    });
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            password: '',
+        },
+        validationSchema: formSchema,
+        onSubmit: (values) => {
+            fetch('/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(values, null, 2),
+            }).then((res) => {
+                if (res.ok) {
+                    res.json().then((user) => onLogin(user));
+                    navigate('../user/home');
+                } else {
+                    Error('invalid credentials');
+                }
+            });
+        },
+    });
+
+    const { errors } = formik;
+    const arrayErrors = Array.from(errors);
     return <div>Signup</div>;
 }
